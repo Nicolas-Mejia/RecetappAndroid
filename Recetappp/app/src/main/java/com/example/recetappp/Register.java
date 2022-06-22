0c2e0efcd437633d10554d2e089928d61e47ff21
nicomejiacerta <nico-mejia@hotmail.com>
2022-06-22 20:09:50 -0300

    firma apiCall

--- Recetappp/app/src/main/java/com/example/recetappp/Register.java
+++ Recetappp/app/src/main/java/com/example/recetappp/Register.java
@@ -3,18 +3,59 @@ package com.example.recetappp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class Register extends AppCompatActivity {
     public ImageButton button;
+    public Button registerButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+
         setContentView(R.layout.activity_register);
 
+
         button = (ImageButton) findViewById(R.id.returnButtonRegister);
+        registerButton = (Button) findViewById(R.id.registerButton);
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +64,15 @@ public class Register extends AppCompatActivity {
             }
         });
 
+            registerButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                        apiCall();
+                }
+            });
+
+    }
+
+    private void apiCall(){
     }
 }
